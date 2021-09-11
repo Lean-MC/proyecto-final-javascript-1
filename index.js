8b025a730cda44bbc0af294110fd551a3a800982
Lean-MC <87346541+Lean-MC@users.noreply.github.com>
2021-09-11 15:46:41 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+
+let nombreCliente = prompt("ingresa tu nombre para una mejor experiencia")
+alert(`Bienvenido ${nombreCliente}`)
+alert(`${nombreCliente} a continuacion podras ver todas las viandas que tenemos para vos`)
+let mailCliente = prompt("ingresa tu mail para recibir nuestras novedades")
+alert(`${nombreCliente} te estaremos notificando a ${mailCliente} nuestras ofertas y nuevas viandas gracias y buen provecho`)
+
